2bb430659bd1226b113e79e2bec6b41999d55542
tylapaul <pauliustyla10@gmail.com>
2025-05-29 20:23:28 +0300

    paskutine paskaita

---
+++ python_basic.py
@@ -0,0 +1,22 @@
+print("Valio")
+#komentaras 
+pranesimas = "Labas"
+#pranesimas yra string tipo kintamasis
+print(pranesimas)
+amzius_str = input("Ives amziu: ")  # imput komanda duoda teksta
+amzius = int(amzius_str)
+ugis = 1.80 #uzvedus pele rodo kintamojo tipa 
+Yra_vedes = False
+is_married = False
+is_old_enough = amzius > 18  #boolean
+print(f"Labas, as krabas, taciau net {amzius} senumo")
+print(is_old_enough)
+if Yra_vedes: 
+    print("As vedes") 
+else: print("as nevedes")
+print(pranesimas)
+if amzius <= 10:
+     print("Dusk seni")
+elif amzius >=50:
+     print("gerk vodke")
+else: print("dirbk")
