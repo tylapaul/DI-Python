3207253575688fcfe76b4858a2f84fef8a0018ce
tylapaul <pauliustyla10@gmail.com>
2025-06-04 15:24:56 +0300

    v3

---
+++ functions_03.py
@@ -0,0 +1,28 @@
+import datetime
+
+def get_user_birthdate():
+    while True:
+        birthdate_str = input("Įveskite savo gimimo datą (formatu YYYY-MM-DD): ")
+        try:
+            birthdate = datetime.datetime.strptime(birthdate_str, "%Y-%m-%d").date()
+            break  # Išeiname iš ciklo, jei data sėkmingai konvertuota
+        except ValueError:
+            print("Klaida: Neteisingas datos formatas. Bandykite dar kartą.")
+    today = datetime.date.today()
+
+    if birthdate > today:
+        print("Klaida: Gimimo data negali būti ateityje.")
+        return
+    return birthdate
+
+def calculate_age(birthdate):
+    today = datetime.date.today()
+        # Amžiaus skaičiavimas
+    # Apskaičiuojame skirtumą metais
+    age = today.year - birthdate.year
+
+    # Tiksliname amžių, jei šiais metais gimtadienis dar nebuvo
+    # Tikriname, ar mėnuo yra ankstesnis arba tas pats mėnuo, bet diena ankstesnė
+    if (today.month, today.day) < (birthdate.month, birthdate.day):
+        age -= 1
+    print(f"Jums yra {age} metai(-ų).")
---
+++ namu darbas 0603.py
@@ -0,0 +1,9 @@
+from functions_03 import get_user_birthdate, calculate_age 
+
+result = get_user_birthdate()
+
+print (f"Jei jūs gimėt {result}")
+
+calculate_age (result)
+
+#print(f"Jei jūsų gimtadienis ", {result} , "tada jums X metų")
